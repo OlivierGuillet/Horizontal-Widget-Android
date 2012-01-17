57c2e7d4f588eb2ce4c4ba37a19fbde9cacc1fa4
Olivier Guillet <olguillet@gmail.com>
2012-01-17 03:08:47 +0100

    widget fonctionnel

--- gen/com/olivierguillet/horizontalwidget/android/R.java
+++ gen/com/olivierguillet/horizontalwidget/android/R.java
@@ -12,9 +12,16 @@ public final class R {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
+        public static final int indicator_current=0x7f020001;
+    }
+    public static final class id {
+        public static final int current=0x7f050002;
+        public static final int gallery=0x7f050001;
+        public static final int text=0x7f050000;
     }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int gallery_item=0x7f030000;
+        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
--- src/com/olivierguillet/horizontalwidget/android/HorizontalWidgetActivity.java
+++ src/com/olivierguillet/horizontalwidget/android/HorizontalWidgetActivity.java
@@ -1,13 +1,63 @@
 package com.olivierguillet.horizontalwidget.android;
 
 import android.app.Activity;
+import android.graphics.PixelFormat;
 import android.os.Bundle;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Gallery;
+import android.widget.TextView;
 
+/**
+ * @author Olivier Guillet
+ * 
+ */
 public class HorizontalWidgetActivity extends Activity {
-    /** Called when the activity is first created. */
+
+    /**
+     * @see android.app.Activity#onCreate(android.os.Bundle)
+     */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // gradients plus joli
+        //getWindow().setFormat(PixelFormat.RGBA_8888);
+        //getWindow().addFlags(WindowManager.LayoutParams.FLAG_DITHER);
+
         setContentView(R.layout.main);
+
+        // tableau d'items
+        final String[] tabStr = new String[20];
+        for (int i = 0; i < 20; i++) {
+            tabStr[i] = (new Integer(i + 1)).toString();
+        }
+
+        // composant natif Gallery
+        // une Gallery est un widget affichant une liste d'items de manière horizontale
+        // et positionnant l'élément sélectionné de manière centrée
+        Gallery gallery = (Gallery) this.findViewById(R.id.gallery);
+        ArrayAdapter<String> arr = new ArrayAdapter<String>(this, R.layout.gallery_item, tabStr);
+        gallery.setAdapter(arr);
+
+        gallery.setOnItemSelectedListener(new OnItemSelectedListener() {
+
+            @Override
+            public void onItemSelected(AdapterView parent, View view, int position, long id) {
+                TextView txt = (TextView) findViewById(R.id.text);
+                txt.setText("Selected : " + tabStr[position]);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+                // TODO Auto-generated method stub                
+            }
+
+        });
+
     }
+
 }
